75ea89523fa2954fd88ca1430405fabd4c45885d
Yesmine-Gherri <mina.gh2060@gmail.com>
2024-05-22 21:20:05 +0200

    delete downloads folder

--- Downloads/mappshere/src/app/models/history.ts
+++
@@ -1,6 +0,0 @@
-export interface WeatherHistory {
-    id: string;
-    city: string;
-    weather: number;
-    date: Date;
-}
--- Downloads/mappshere/src/app/models/weather-history-entry.model.ts
+++
@@ -1,6 +0,0 @@
-// src/app/models/weather-history-entry.model.ts
-export interface WeatherHistoryEntry {
-  city: string;
-  temperature: number;
-  weatherIcon: string;
-}
